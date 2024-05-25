76f3ac276095d5244fad82a56a7a1bcdb3b39609
Vinzgh24 <128291675+Vinzgh24@users.noreply.github.com>
2024-05-25 13:11:32 +0700

    Update mediainfo.py

--- TelegramBot/plugins/mediainfo.py
+++ TelegramBot/plugins/mediainfo.py
@@ -129,7 +129,7 @@ async def ddl_mediainfo(message, url, isRaw):
         download_path = os.path.join(download_dir, f"{rand_str}_{filename}")
         
         # Initiating Httpx client
-        async with httpx.AsyncClient() as client:
+        client = httpx.AsyncClient() 
             headers = {"user-agent": "Mozilla/5.0 (Linux; Android 12; 2201116PI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"}
             
         # Trigger TimeoutError after 15 seconds if download is slow / unsuccessful 
